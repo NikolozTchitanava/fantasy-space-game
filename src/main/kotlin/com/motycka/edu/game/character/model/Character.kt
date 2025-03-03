ccaf8dfb1deb013e68f1321a955d1ee3f3244481
NikolozTchitanava <143025948+NikolozTchitanava@users.noreply.github.com>
2025-03-03 07:13:31 +0700

    added CharacterClass.kt

--- src/main/kotlin/com/motycka/edu/game/character/model/Character.kt
+++ src/main/kotlin/com/motycka/edu/game/character/model/Character.kt
@@ -1,4 +1,22 @@
 package com.motycka.edu.game.character.model
 
-class Character {
-}
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "characters")
+data class Character(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+    val name: String,
+    val health: Int,
+    val attackPower: Int,
+    val stamina: Int?,
+    val defensePower: Int?,
+    val mana: Int?,
+    val healingPower: Int?,
+    @Enumerated(EnumType.STRING)
+    val characterClass: CharacterClass,
+    val level: Int = 1,
+    val experience: Int = 0,
+    val accountId: Long
+)
